9f2650a78179cb3f290d4eadbccfca737ba45208
Corneliu Laur <cl179971@gmail.com>
2023-03-07 04:46:48 +0000

    installed axios and created the App functionality to get the asyncronous data from the API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import { Axios } from 'axios';
 import './App.css';
 
 function App() {
+    const [joke, setJoke] = useState('');
+
+    const getJoke = async () => {
+        const response = await Axios.get(
+            'https://api.chucknorris.io/jokes/random'
+        );
+        setJoke(response.data.value);
+    };
+
     return (
         <div className='App'>
-            <header className='App-header'>Chuck Norris</header>
+            <header className='App-header'>
+                <h1>Press the button for a joke...</h1>
+                <button className='App-button' onClick={getJoke}>
+                    Click Me!
+                </button>
+                <p className='App-card'>{joke}</p>
+            </header>
         </div>
     );
 }
